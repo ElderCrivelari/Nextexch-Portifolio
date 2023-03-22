be635fdcbda27984257948d8cfd66553a234878b
Elder Crivelari Teixeira <elder_436@hotmail.com>
2023-03-22 07:24:19 +0044

    Adicionar arquivos de projeto.

---
+++ CRUD MVC - Portifolio/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUD_MVC___Portifolio.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
